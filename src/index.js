5c439f5266883f559213e0c3f92d8e91a43ee31f
Pavel-Yablonski <yablonski.pavel81@gmail.com>
2021-01-26 00:50:24 +0300

    init: brackets task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const list = [], state = [];
+
+  let flag = true;
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    list[i] = bracketsConfig[i].join('');
+  }
+  
+  while (flag) {
+    for (let j = 0; j < list.length; j++) {
+        if(str.indexOf(list[j]) == -1) {
+            state.push(0);       
+        } else {
+            str = str.replace(list[j],'');                
+            state.push(1);   
+        } 
+    }    
+    if (state.indexOf(1) == -1) {
+        flag = false;
+    }      
+    state = [];
+  }
+  return str.length == 0 ? true : false;
 }
